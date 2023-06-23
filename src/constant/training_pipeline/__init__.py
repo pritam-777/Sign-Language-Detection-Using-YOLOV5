51be0db1b398bd27708ef410e27a3776b3e70814
pritambhakta90@gmail.com <pritambhakta90@gmail.com>
2023-06-23 20:16:46 +0530

    update file

--- src/constant/training_pipeline/__init__.py
+++ src/constant/training_pipeline/__init__.py
@@ -10,7 +10,21 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 
-DATA_DOWNLOAD_URL: str = "https://github.com/entbappy/Branching-tutorial/blob/master/Sign_language_data.zip"
+DATA_DOWNLOAD_URL: str = "https://github.com/pritam-777/PPT-Assignment-/raw/main/Sign_language_data.zip"
+
+
+"""
+Data Validation realted contant start with DATA_VALIDATION VAR NAME
+"""
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+
+DATA_VALIDATION_STATUS_FILE = 'status.txt'
+
+DATA_VALIDATION_ALL_REQUIRED_FILES = ["train", "test", "data.yaml"]
+
+
+
 
 
 
